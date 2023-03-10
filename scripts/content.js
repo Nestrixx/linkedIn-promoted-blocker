562fa25a5f8fd735e8258a79a8708fd60a68b169
crimtin <christianmck@hotmail.com>
2023-03-10 02:35:49 -0600

    set up an observer for watching changes in dom. replacing ads with logo works now.

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,19 @@
 setTimeout(() => {
+    replaceAds();
+    const targetNode = document.querySelector(".scaffold-layout__list-container");
 
+    const config = { attributes: false, childList: true, subtree: true };
+
+    const callback = (mutationList) => {
+        replaceAds();
+    }
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(targetNode, config);
+
+}, 5000)
+const replaceAds = () => {
     const jobListContainer = document.querySelector(".scaffold-layout__list-container");
 
     const jobsList = jobListContainer.querySelectorAll(".jobs-search-results__list-item")
@@ -7,29 +21,13 @@ setTimeout(() => {
     for (let i = 0; i < jobsList.length; i++) {
         const currentJob = jobsList[i];
         const currentJobListElements = currentJob.getElementsByTagName("li");
-        // console.log(jobsList.length, currentJobListElements.length);
 
         for (let j = 0; j < currentJobListElements.length; j++) {
             if (currentJobListElements[j].innerText.toLocaleLowerCase() === "promoted") {
                 const catImageTest = new Image(486, 107)
-                catImageTest.src = chrome.runtime.getURL("../images/cat.jpg");
+                catImageTest.src = chrome.runtime.getURL("../images/logo.png");
                 currentJob.replaceChildren(catImageTest);
-                currentJob.appendChild
             }
         }
-        // for(let j = 0; j < currentJob.length)
-
     }
-
-}, 4000)
-
-
-/*
-
-
-1. loop through JobsList for list items and look innnerHtml of "promoted" for deleting.
-2. remove all child elements with replaceChildren this should remove all children thus removing unwanted list items.
-2.5 replace with cat pic in image tag. this is 2.5 because replace happens same time as child killing.
-
-
-*/
+}
