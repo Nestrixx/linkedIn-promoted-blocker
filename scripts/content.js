541078f7285af6db30086adf663484c9064c55c8
crimtin <christianmck@hotmail.com>
2023-03-15 01:47:10 -0500

    got rid of the timeout. cleaned up code.

--- scripts/content.js
+++ scripts/content.js
@@ -1,33 +1,27 @@
-setTimeout(() => {
-    replaceAds();
-    const targetNode = document.querySelector(".scaffold-layout__list-container");
+const newConfig = { attributes: false, childList: true, subtree: true }
 
-    const config = { attributes: false, childList: true, subtree: true };
-
-    const callback = (mutationList) => {
-        replaceAds();
-    }
-
-    const observer = new MutationObserver(callback);
-
-    observer.observe(targetNode, config);
-
-}, 5000)
 const replaceAds = () => {
-    const jobListContainer = document.querySelector(".scaffold-layout__list-container");
+    const jobList = document.querySelector(".scaffold-layout__list-container")
+    if (jobList) {
+        const jobListContainer = document.querySelector(".scaffold-layout__list-container");
 
-    const jobsList = jobListContainer.querySelectorAll(".jobs-search-results__list-item")
+        const jobsList = jobListContainer.querySelectorAll(".jobs-search-results__list-item")
 
-    for (let i = 0; i < jobsList.length; i++) {
-        const currentJob = jobsList[i];
-        const currentJobListElements = currentJob.getElementsByTagName("li");
+        for (let i = 0; i < jobsList.length; i++) {
+            const currentJob = jobsList[i];
+            const currentJobListElements = currentJob.getElementsByTagName("li");
 
-        for (let j = 0; j < currentJobListElements.length; j++) {
-            if (currentJobListElements[j].innerText.toLocaleLowerCase() === "promoted") {
-                const catImageTest = new Image(486, 107)
-                catImageTest.src = chrome.runtime.getURL("../images/logo.png");
-                currentJob.replaceChildren(catImageTest);
+            for (let j = 0; j < currentJobListElements.length; j++) {
+                if (currentJobListElements[j].innerText.toLocaleLowerCase() === "promoted") {
+                    const catImageTest = new Image(486, 107)
+                    catImageTest.src = chrome.runtime.getURL("../images/logo.png");
+                    currentJob.replaceChildren(catImageTest);
+                }
             }
         }
     }
 }
+
+// this could be an arrow function inside of the Mutation Observer.
+const observer = new MutationObserver(replaceAds);
+observer.observe(document, newConfig);
